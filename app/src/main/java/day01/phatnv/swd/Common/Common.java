45224903ff600328db5c19c473fd72e0eae0475f
Phat <lanceyukan1997@gmail.com>
2018-11-11 15:25:25 +0700

    push code

---
+++ app/src/main/java/day01/phatnv/swd/Common/Common.java
@@ -0,0 +1,7 @@
+package day01.phatnv.swd.Common;
+
+import day01.phatnv.swd.Model.User;
+
+public class Common {
+    public static User currentUser;
+}
---
+++ app/src/main/java/day01/phatnv/swd/MainActivity.java
@@ -0,0 +1,56 @@
+package day01.phatnv.swd;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.oob.SignUp;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn,btnSignUp;
+    TextView txtSlogan;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn=(Button)findViewById(R.id.btnSignIn);
+        btnSignUp=(Button)findViewById(R.id.btnSignUp);
+
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/NABILA.TTF");
+        txtSlogan.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent signUp = new Intent(MainActivity.this, day01.phatnv.swd.SignUp.class);
+                startActivity(signUp);
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signIn= new Intent(MainActivity.this,SignIn.class);
+                startActivity(signIn);
+            }
+        });
+    }
+}
+
